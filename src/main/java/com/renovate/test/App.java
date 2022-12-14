f1e01fa166941add88a8334562c61b2ee81ee72d
Joseph Jennings <Joseph.Jennings@oracle.com>
2022-12-14 15:59:31 -0600

    Add renotest project

---
+++ src/main/java/com/renovate/test/App.java
@@ -0,0 +1,13 @@
+package com.renovate.test;
+
+/**
+ * Hi world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi World!" );
+    }
+}
